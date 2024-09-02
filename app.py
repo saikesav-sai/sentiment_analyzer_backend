21f4c3f9e452a8a436748030d169d41f93e8ddfd
saikesav-sai <saikesav6725@gmail.com>
2024-09-02 11:58:03 +0530

    testing without tensorflow

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from tensorflow.keras.models import load_model
-from keras.preprocessing.sequence import pad_sequences
-import numpy as np
-import pickle
+# from tensorflow.keras.models import load_model
+# from keras.preprocessing.sequence import pad_sequences
+# import numpy as np
+# import pickle
 import os
 
 
@@ -11,12 +11,12 @@ keras_file          =os.path.join(os.path.dirname(__file__), 'model.keras')
 tokenizer_file      =os.path.join(os.path.dirname(__file__), 'tokenizer.pkl')
 label_encoder_file  =os.path.join(os.path.dirname(__file__), 'label_encoder.pkl')
 
-# Load the saved model and tokenizer
-rnn_model = load_model(keras_file)
-with open(tokenizer_file, 'rb') as f:
-    tokenizer = pickle.load(f)
-with open(label_encoder_file, 'rb') as f:
-    label_encoder = pickle.load(f)
+# # Load the saved model and tokenizer
+# rnn_model = load_model(keras_file)
+# with open(tokenizer_file, 'rb') as f:
+#     tokenizer = pickle.load(f)
+# with open(label_encoder_file, 'rb') as f:
+#     label_encoder = pickle.load(f)
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -29,16 +29,18 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    review = data['review']
+    # review = data['review']
 
-    # Preprocess the input review
-    sequence = tokenizer.texts_to_sequences([review])
-    padded_sequence = pad_sequences(sequence, maxlen=300)
+    # # Preprocess the input review
+    # sequence = tokenizer.texts_to_sequences([review])
+    # padded_sequence = pad_sequences(sequence, maxlen=300)
 
-    # Make prediction
-    prediction = rnn_model.predict(padded_sequence)
-    predicted_label = np.argmax(prediction, axis=1)
-    class_label = label_encoder.inverse_transform(predicted_label)
+    # # Make prediction
+    # prediction = rnn_model.predict(padded_sequence)
+    # predicted_label = np.argmax(prediction, axis=1)
+    # class_label = label_encoder.inverse_transform(predicted_label)
+    class_label=[]
+    class_label.append("sai kesav")
 
     return jsonify({'predicted_label': class_label[0]})
 
